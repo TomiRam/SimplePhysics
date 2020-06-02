614749f03ab49d09d1ae0382c198d593963f664e
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-06-02 13:54:29 +0300

    Create Physics.cs

---
+++ Physics.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Simple_Physics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m;
+            int a;
+            m = 100; //Enter objects Mass 
+            a = 2; //Enter objects Accelaration
+            Write(" F="); 
+            Write(m * a);
+            Write("N");
+            ReadLine();
+
+        }
+    }
+}
